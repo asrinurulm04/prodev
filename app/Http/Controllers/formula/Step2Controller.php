6e1ce6360d9cc7fd2f21adb08f0e31e3abd5a01d
Asri Nurul Ma'rifah <asri.nurul@nutrifood.co.id>
2020-10-20 15:17:46 +0700

    detail alternatif bahan baku, penambahan premix, hitung koma

--- app/Http/Controllers/formula/Step2Controller.php
+++ app/Http/Controllers/formula/Step2Controller.php
@@ -13,6 +13,7 @@ use App\dev\Premix;
 use App\dev\Pretail;
 use App\pkp\tipp;
 use App\dev\Bahan;
+use App\dev\bb_allergen;
 use Auth;
 use DB;
 use Redirect;
@@ -25,11 +26,10 @@ class Step2Controller extends Controller
         $this->middleware('rule:user_rd_proses' || 'rule:user_produk');
     }
     
-    public function create($id){
-        
-
-        $formula = Formula::where('workbook_id',$id)->first();
-        $wb = tipp::where('id',$id)->first();
+    public function create($formula,$id){
+        $formula = Formula::where('id',$id)->first();
+        //dd($formula);
+        $wb = tipp::where('id_pkp',$id)->first();
         $target_serving = $formula->serving_size;
 
         // checkbase !
@@ -41,7 +41,9 @@ class Step2Controller extends Controller
         }
 
         $idf = $id;
+        $idfor = $formula->workbook_id;
         $fortails = Fortail::where('formula_id',$id)->get();
+        //dd($fortails);
         $ada= Fortail::where('formula_id',$id)->count();
         $bahans = Bahan::all();
         $no = 0;
@@ -53,16 +55,27 @@ class Step2Controller extends Controller
         $rjsBatch   = 0;
         $rjsServing = 0;
         $granulasi  = 0;
+        $premix  = 0;
         foreach($fortails as $fortail){
             $scalecollect->push([
                 
                 'no' => ++$no,                
-                'id' => $fortail->id,
+                'id' => $fortail->id,      
+                'formula_id' => $fortail->formula_id,
                 'nama_sederhana' => $fortail->nama_sederhana,
+                'alternatif' => $fortail->alternatif,
+                'alternatif2' => $fortail->alternatif2,
+                'alternatif3' => $fortail->alternatif3,
+                'alternatif4' => $fortail->alternatif4,
+                'alternatif5' => $fortail->alternatif5,
+                'alternatif6' => $fortail->alternatif6,
+                'alternatif7' => $fortail->alternatif7,
+                'nama_bahan' => $fortail->nama_bahan,
                 'per_batch' => $fortail->per_batch,
                 'per_serving' => $fortail->per_serving,
                 'scale_batch' => '',
                 'scale_serving' => '',
+                'premix' => $fortail->premix,   
                 'granulasi' => $fortail->granulasi                
             ]);
             $rjBatch   = $rjBatch + $fortail->per_batch;
@@ -71,7 +84,13 @@ class Step2Controller extends Controller
             // Granulasi
             if($fortail->granulasi == 'ya'){
             $granulasi = $granulasi + 1;  
-            }                                                         
+            //dd($granulasi);        
+            }      
+            // Premix
+            if($fortail->premix == 'ya'){
+                $premix = $premix + 1;  
+                 //dd($premix);        
+            }                             
         }
 
         // Check Total Serving
@@ -81,7 +100,6 @@ class Step2Controller extends Controller
         else{
             $sesuai_target = 0;
         }  
-        
         return view('formula/step2')->with([
             'target_serving' => $target_serving,
             'formula' => $formula,
@@ -89,18 +107,26 @@ class Step2Controller extends Controller
             'fortails' => $fortails,
             'scalecollect' => $scalecollect,
             'bahans' => $bahans,
-            'granulasi' => $granulasi,            
+            'idfor' => $idfor,
+            'granulasi' => $granulasi,    
+            'premix' => $premix,         
             'idf' => $idf,
             'ada' => $ada,
             'sesuai_target' => $sesuai_target
-            ]);
-        }
+        ]);
+    }
+
+    public function update($formula,$id,Request $request){
+        $formula = Formula::where('id',$formula)->first();
+        $formula->catatan_rd = $request->keterangan;
+        $formula->save();
+        
+        return Redirect()->back();
+    }
 
     public function insert($vf,Request $request){
-
         // Check and Recheck
-            $formula = Formula::where('workbook_id',$vf)->first();
-
+            $formula = Formula::where('id',$vf)->first();
         // checkbase !
             if($formula->batch != null){
                 $mybase = $formula->batch / $formula->serving;
@@ -130,27 +156,59 @@ class Step2Controller extends Controller
         if($c>0){
             for($d = 1;$d<=$c;$d++){
                 $ba[$d] =  Bahan::where('id',$request->alternatif[$d])->first();
-                $pkk = $pkk.' / '.$ba[$d]->kode_komputer;
+                //$pkk = $pkk.' / '.$ba[$d]->kode_komputer;
                 // $pin = $pin.' / '.$ba[$d]->id_ingredient;
-                $pns = $pns.' / '.$ba[$d]->nama_sederhana;
-                $pko = $pko.' / '.$ba[$d]->kode_oracle;
-                $pnb = $pnb.' / '.$ba[$d]->nama_bahan;
+                // $pns = $pns.' / '.$ba[$d]->nama_sederhana;
+                // $pko = $pko.' / '.$ba[$d]->kode_oracle;
+                // $pnb = $pnb.' / '.$ba[$d]->nama_bahan;
                 $e=$d+1;
                 
-                $nk = 'kode_komputer'.$e;
-                $fortails->$nk = $ba[$d]->id;
             }
         }
         
         $fortails->formula_id = $vf;
-        $fortails->kode_komputer = $pkk;
+        $fortails->kode_komputer = $bp->kode_komputer;
         $fortails->id_ingredient = $pin;
-        $fortails->nama_sederhana = $pns;
+        $fortails->kode_oracle = $bp->kode_oracle;
+        $fortails->nama_sederhana = $bp->nama_sederhana;
+        $fortails->principle = $bp->principle;
         $fortails->kode_oracle = $pko;
         $fortails->bahan_id = $bp->id;
-        $fortails->nama_bahan = $pnb;
+        $fortails->nama_bahan = $bp->nama_bahan;
         if($c>0){
             $fortails->alternatif= $ba[1]->nama_sederhana;
+            $fortails->nama_bahan1= $ba[1]->nama_bahan;
+            $fortails->principle1= $ba[1]->principle;
+            if($c>1){
+            $fortails->alternatif2= $ba[2]->nama_sederhana;
+            $fortails->nama_bahan2= $ba[2]->nama_bahan;
+            $fortails->principle2= $ba[2]->principle;
+            }
+            if($c>2){
+            $fortails->alternatif3= $ba[3]->nama_sederhana;
+            $fortails->nama_bahan3= $ba[3]->nama_bahan;
+            $fortails->principle3= $ba[3]->principle;
+            }
+            if($c>3){
+            $fortails->alternatif4= $ba[4]->nama_sederhana;
+            $fortails->nama_bahan4= $ba[4]->nama_bahan;
+            $fortails->principle4= $ba[4]->principle;
+            }
+            if($c>4){
+            $fortails->alternatif5= $ba[5]->nama_sederhana;
+            $fortails->nama_bahan5= $ba[5]->nama_bahan;
+            $fortails->principle5= $ba[5]->principle;
+            }
+            if($c>5){
+            $fortails->alternatif6= $ba[6]->nama_sederhana;
+            $fortails->nama_bahan6= $ba[6]->nama_bahan;
+            $fortails->principle6= $ba[6]->principle;
+            }
+            if($c>6){
+            $fortails->alternatif7= $ba[7]->nama_sederhana;
+            $fortails->nama_bahan7= $ba[7]->nama_bahan;
+            $fortails->principle7= $ba[7]->principle;
+            }
         }        
         
         // if($request->per_batch >= 3000){
@@ -192,6 +250,13 @@ class Step2Controller extends Controller
                 $fortails->granulasi = 'ya';                
             }else{
                 $fortails->granulasi = 'tidak';
+            }
+
+            // Premix
+            if (isset($request->cpremix)){
+                $fortails->premix = 'ya';                
+            }else{
+                $fortails->premix = 'tidak';
             }
                 
         $fortails->save();
@@ -293,11 +358,13 @@ class Step2Controller extends Controller
 
         }
         
-        return redirect()->route('step2',$formula->workbook_id)->with('status','BahanBaku Berhasil Ditambahkan');
+        return redirect()->route('step2',['id_workbook' => $vf, 'id_formula' => $formula->id])->with('status','BahanBaku Berhasil Ditambahkan');
     }
 
     public function destroy($id,$vf){
-        $formula = Formula::where('workbook_id',$vf)->first();
+        //dd($id);
+        $formula = Formula::where('id',$vf)->first();
+        $idfor = $formula->workbook_id;
         $fortail = Fortail::where([
             ['id',$id],
             ['formula_id',$vf]
@@ -321,7 +388,7 @@ class Step2Controller extends Controller
                 $premix->delete();
                 }
             $fortail->delete();
-        return redirect()->route('step2',$vf)->with('error','BahanBaku Telah Berhasil Dihapus');
+        return redirect()->route('step2',[$idfor,$vf])->with('error','BahanBaku Telah Berhasil Dihapus');
     }
 
 }
