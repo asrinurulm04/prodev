411561340e1629a63754789081e4c2ea54a4cf93
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2022-02-08 09:56:00 +0700

    [other] notif email admin

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\model\users\Departement;
 use App\model\users\Role;
 use App\model\users\User;
+use DB;
 
 class RegistrationController extends Controller
 {
@@ -38,6 +39,29 @@ class RegistrationController extends Controller
         $user->role_id        = $request->role;
         $user->save();
         
+        $role = Role::where('id',$request->role)->first();
+        try{ // memberi informasi kepada admin jika terdapat pengajuan user barus
+            Mail::send('email.user', [
+                'nama'  => $request->name,
+                'email'  => $request->email,
+                'roles'  => $role,
+            ], function ($message) use ($request) {
+                $dept  = Departement::where('id',$request->departement)->first(); //manager dari dept terkait
+                $user1 = $request->email; // user yang mengajuan
+                $user2 = User::where('id',$dept->manager_id)->first();
+                $user = DB::table('tr_users')->where('role_id','4')->get(); //mencaru user admin
+                $message->subject('PRODEV | Pengajuan User Baru!');
+                foreach($user as $user){
+                    $data = $user->email;
+                    $message->to($data);
+                }
+                $message->cc($user1,$user2->email);
+            });
+        }
+        catch (Exception $e){
+            return response (['status' => false,'errors' => $e->getMessage()]);
+        }
+
         return redirect()->to('/signin')->with('status', 'Add Data Success ');
     }
 }
---
+++ resources/views/email/user.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+	<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	</head>
+	<body class="">
+		<table border="0" cellpadding="0" cellspacing="0" class="body">
+    	<tr>
+        <td>
+          <p>Hai Admin,</p>
+          <p>Terdapat Pengajuan Akun Baru, Mohon Untuk Ditindak lanjuti Segera!.</p></p>
+          <p>Dengan Info Pengajuan Sebagai Berikut :</p>
+					<div class="container"><hr>
+					<table border="0" cellpadding="0" cellspacing="0">
+            <tr><td>Nama User</td><td>: {{$nama}}</td></tr>
+            <tr><td>Email</td><td>: {{$email}}</td></tr>
+            <tr><td>Role yang diajukan</td><td>: {{$roles->role}}</td></tr>
+					</table> <hr><br><br>
+					Terimakasih,<br>
+					PRODEV
+          </div>
+        </td>
+    	</tr>
+		</table>
+	</body>
+</html>
